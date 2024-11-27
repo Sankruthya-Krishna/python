c4dea7f4ca8887a738757ad66813b9eb9441f891
Sankruthya-Krishna <sankruthyakrishna13@gmail.com>
2024-11-27 12:46:38 +0530

    basic programs in pyhton

---
+++ first.py
@@ -0,0 +1,60 @@
+print("hello world my first program in python")
+# name="honey"
+# age=21
+# a= None
+# x=2
+# y=6
+# val1=True
+# val2=False
+# sum=x+y
+# sub=y-x
+# mul=x*y
+# div=x/y
+# #print("sum is:",sum)
+# print("ans operator:",val1 and val2)
+# print("ans operator:",val1 or val2)
+# print("ans operator:",x and y)
+# print("my name is",name,"my age is",age)
+# print(type(name))
+# print(type(a))
+# #Arithmetic operation
+# print("Arithmetic operation of sum is:",sum,"subtraction is",sub,"multiplication is",mul,"division is:",div)
+# print(x+y)
+# print(x %y)# remainder
+
+# print(x**y)#x power y
+# #relational/comparion operators
+# print(x==y)
+# print(x!=y)
+# print(x<=y)
+# print(x>=y)
+# #type conversion
+# a=2
+# b=4.3
+# print(a+b)
+# #input
+# name=input("enter your name:")
+# print("hello",name)
+# w=str(input("enter your name:"))
+# print(type(w),w)
+#first program(sum of two numbers)
+a=int (input("enter first numbe"))
+b=int (input("enter second number"))
+sum=a+b
+print("sum of twp numbers:",sum)
+#second program(Area of square)
+s=int(input("enter side of a square"))
+area=s*s
+print("area of square:",area)
+#third program(avg of two floating numbers)
+val1=float(input("enter a number"))
+val2=float(input("enter a number"))
+avg=(val1+val2)/2
+print("avg of two floating numbers:",avg)
+#fourth program(greatest of 2 numbers)
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+# if a>=b:print("True")
+# else:print("False")
+print(a>=b)#--> it will directly print true if condition matches else false
